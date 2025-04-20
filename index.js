67f8271602cddc447f794f622f3833eef470f41d
Vansh <vanshrotkar15@gmail.com>
2025-04-20 13:36:53 +0530

    Final Changes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 const port = 3000;
+const dbPassword = process.env.DB_PASSWORD;
+
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "Blog",
-  password: "Vansh@1505",
+  password: dbPassword,
   port: 5432,
 });
 db.connect();
